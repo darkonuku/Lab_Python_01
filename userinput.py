92ec84145eb5f9cf231acdba5a8560d3caeafd42
Guest <guest-lCFJrK@AITI-75.(none)>
2012-06-20 13:23:12 +0000

    Lab01 exercise 4

--- userinput.py
+++ userinput.py
@@ -1,3 +1,15 @@
 print'EXERCISE 4'
 print''
-firs
+
+#prompting the user to enter his firstname and lastname
+first_name = raw_input('Enter your first name: ')
+last_name = raw_input('Enter your last name: ')
+
+#prompting the user to enter the date of birth
+print'Enter your date of birth: '
+month = raw_input('\tMonth?: ')
+day = raw_input('\tDay?')
+year = raw_input('\tYear?')
+
+#printing the name and date of birth of the user
+print first_name, last_name, 'is born on', month, day + ',', year
